c5b6b463f7ff63ef7698543a91be213edb31dd67
navabhi22 <navabhi22@gmail.com>
2022-02-25 21:19:36 +0530

    Abhinav Add Create New Contact

---
+++ AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBook
+    {
+        /// <summary>
+        /// Creates New Contact.
+        /// </summary>
+        public void AddrsBook()
+        {
+            Console.WriteLine("A to Add Person");
+            Console.WriteLine("Q to Quit");
+            string UserInput = Console.ReadLine();
+            switch (UserInput)
+            {
+                case "A":
+                    CreateContact contact = new CreateContact();
+                    contact.CreateContacts();
+                    AddressBook addressBook = new AddressBook();
+                    addressBook.AddrsBook();
+                    break;
+                case "Q":
+                    break;
+                default:
+                    Console.WriteLine("Choose Valid Option");
+                    break;
+            }
+        }
+
+    }
+}
