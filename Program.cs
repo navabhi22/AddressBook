cc38539204fdf913960f62a0298ec8e73918ec83
navabhi22 <navabhi22@gmail.com>
2022-02-25 21:03:49 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("AddressBook is empty Enter the number of contacts you want to create");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                CreateContact contact = new CreateContact();
+                contact.CreateContacts();
+            }
         }
     }
 }
