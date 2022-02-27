eb74f4917467bc3b8a236c3ed8c99c48e101e0b4
navabhi22 <navabhi22@gmail.com>
2022-02-27 22:50:26 +0530

    Abhinav Added AssMultiple.cs

---
+++ AddMultiple.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddMultiple
+    {
+        /// <summary>
+        /// Creates Multiple Contact.
+        /// </summary>
+        public void AddNewContact()
+        {
+
+            Console.WriteLine("A to Add Person");
+            Console.WriteLine("Q to Quit");
+            string UserInput = Console.ReadLine();
+            switch (UserInput)
+            {
+                case "A":
+                    CreateContact contact = new CreateContact();
+                    contact.CreateContacts();
+                    AddressBook addressBook = new AddressBook();
+                    addressBook.AddrsBook();
+                    break;
+                case "Q":
+                    break;
+                default:
+                    Console.WriteLine("Choose Valid Option");
+                    break;
+            }
+        }
+
+    }
+}
